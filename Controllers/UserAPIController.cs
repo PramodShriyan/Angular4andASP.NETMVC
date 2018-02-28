04692b23e178404bbd83912536a66ca0def07388
Pramod Shriyan <32896559+PramodShriyan@users.noreply.github.com>
2018-02-28 14:40:42 +0530

    Add files via upload

---
+++ Controllers/UserAPIController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using Angular2MVC.DBContext;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Angular2MVC.Controllers
+{
+    public class UserAPIController : ApiController
+    {
+        protected readonly UserDBEntities UserDB = new UserDBEntities();
+        public HttpResponseMessage Get()
+        {
+            return ToJson(UserDB.TblUsers.AsEnumerable());
+        }
+
+        public HttpResponseMessage Post([FromBody]TblUser value)
+        {
+            UserDB.TblUsers.Add(value);
+            return ToJson(UserDB.SaveChanges());
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]TblUser value)
+        {
+            UserDB.Entry(value).State = EntityState.Modified;
+            return ToJson(UserDB.SaveChanges());
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            UserDB.TblUsers.Remove(UserDB.TblUsers.FirstOrDefault(x => x.Id == id));
+            return ToJson(UserDB.SaveChanges());
+        }
+
+        public HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
